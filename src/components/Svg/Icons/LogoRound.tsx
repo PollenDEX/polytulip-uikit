f72b5423a70778174b8471c77d8ed27cac09acef
PollenDEX <87180875+PollenDEX@users.noreply.github.com>
2021-07-09 15:41:53 +0200

    Initial commit

---
+++ src/components/Svg/Icons/LogoRound.tsx
@@ -0,0 +1,194 @@
+import React from "react";
+import Svg from "../Svg";
+import { SvgProps } from "../types";
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 512 512" {...props}>
+    <sodipodi:namedview
+       id="namedview7"
+       pagecolor="#ffffff"
+       bordercolor="#666666"
+       borderopacity="1.0"
+       inkscape:pageshadow="2"
+       inkscape:pageopacity="0"
+       inkscape:pagecheckerboard="false"
+       inkscape:document-units="mm"
+       showgrid="false"
+       inkscape:zoom="1.5001964"
+       inkscape:cx="301.62717"
+       inkscape:cy="521.26508"
+       inkscape:window-width="1920"
+       inkscape:window-height="1046"
+       inkscape:window-x="-11"
+       inkscape:window-y="-11"
+       inkscape:window-maximized="1"
+       inkscape:current-layer="layer1" />
+    <defs
+       id="defs2">
+      <inkscape:path-effect
+         effect="spiro"
+         id="path-effect740"
+         is_visible="true"
+         lpeversion="1" />
+    </defs>
+    <g
+       inkscape:label="Layer 1"
+       inkscape:groupmode="layer"
+       id="layer1">
+      <rect
+         style="fill:#77d6eb;fill-opacity:1;stroke:none;stroke-width:0.19891;stroke-opacity:1"
+         id="rect16581"
+         width="122.96352"
+         height="88.294296"
+         x="36.165741"
+         y="92.783829" />
+      <rect
+         style="fill:#000000;stroke:none;stroke-width:0.264583;stroke-opacity:1;fill-opacity:0"
+         id="rect9827"
+         width="162.3717"
+         height="99.767563"
+         x="23.195957"
+         y="87.546036"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#ffffff;stroke:none;stroke-width:0.547749;stroke-opacity:1"
+         id="path7843-6"
+         cx="72.064774"
+         cy="145.87169"
+         rx="26.099846"
+         ry="23.382822"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#ffffff;stroke:none;stroke-width:0.467493;stroke-opacity:1"
+         id="path7843"
+         cx="68.919899"
+         cy="119.94236"
+         rx="21.620243"
+         ry="20.561728"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#ffffff;stroke:none;stroke-width:0.433683;stroke-opacity:1"
+         id="path7843-8"
+         cx="90.902473"
+         cy="117.37298"
+         rx="20.328497"
+         ry="18.819466"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <path
+         style="fill:none;stroke:#000000;stroke-width:0.264583px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+         d="m 86.922485,166.73653 h 45.643655 l 8.60495,-15.71339 -54.248605,-41.27882 -24.628281,23.03301 z"
+         id="path3848"
+         sodipodi:nodetypes="cccccc"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#c8ab37;stroke:#000000;stroke-width:0.264583;stroke-opacity:1"
+         id="path6035"
+         cx="93.738426"
+         cy="132.80344"
+         rx="6.0846195"
+         ry="5.5555229"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <rect
+         style="fill:#1a1a1a;stroke:#000000;stroke-width:0.264583;stroke-opacity:1"
+         id="rect6125"
+         width="9.8323927"
+         height="3.0423098"
+         x="88.77813"
+         y="131.32639"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <path
+         style="fill:#803300;stroke:#000000;stroke-width:0.33329;stroke-opacity:1"
+         d="m 303.29362,594.19423 c -14.11482,-19.46387 -34.92116,-48.14736 -46.23631,-63.74108 -11.31515,-15.59373 -20.53894,-28.44896 -20.49732,-28.5672 0.0416,-0.11823 20.76699,-19.5641 46.05636,-43.21305 l 45.98066,-42.99808 4.01279,3.07965 c 2.20704,1.6938 45.1824,34.39887 95.50081,72.67792 50.31841,38.27905 91.82598,69.8593 92.23905,70.17833 0.74687,0.57684 0.71556,0.6017 -5.66593,4.49739 -3.52932,2.15454 -6.59403,4.0823 -6.81047,4.28392 -0.29518,0.27496 1.59916,2.44875 7.58057,8.69881 4.38575,4.58273 7.93739,8.50252 7.89254,8.71063 -0.0448,0.20812 -5.18088,9.69268 -11.4134,21.07679 l -11.33185,20.6984 -85.8221,0.003 -85.82209,0.003 -25.66331,-35.38886 z m 57.65912,-71.43803 c 1.09985,-0.29235 3.27457,-1.15479 4.8327,-1.91652 2.25763,-1.1037 3.41231,-1.96261 5.68569,-4.22929 4.50651,-4.49325 6.31274,-8.69244 6.31274,-14.67611 0,-5.90156 -1.82089,-10.19398 -6.15024,-14.4981 -2.97929,-2.96194 -5.33207,-4.48904 -8.73628,-5.67037 -7.1152,-2.46914 -15.15436,-1.75597 -21.38693,1.89726 -1.03852,0.60874 -3.0951,2.30663 -4.57016,3.77311 -4.35239,4.32702 -6.15024,8.54719 -6.15024,14.43669 0,5.95151 1.79645,10.21282 6.15024,14.58882 3.69664,3.7155 7.79328,5.86712 12.92645,6.78918 2.39999,0.4311 8.65289,0.1521 11.08603,-0.49467 z"
+         id="path6401"
+         transform="scale(0.26458333)"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <path
+         style="fill:#333333;stroke:#000000;stroke-width:0.33329;stroke-opacity:1"
+         d="m 323.52922,621.50259 c -16.39746,-22.58485 -68.0854,-93.87902 -73.27581,-101.07073 -12.09533,-16.75903 -13.14487,-18.24963 -13.04889,-18.53284 0.11478,-0.3387 -2.1377,1.77945 50.16973,-47.17773 22.59024,-21.14336 41.15063,-38.44246 41.2453,-38.44246 0.0947,0 35.00871,26.50903 77.58674,58.90895 42.57802,32.39992 85.61468,65.13314 95.63701,72.74048 10.02234,7.60733 18.18359,13.9815 18.13611,14.16481 -13.93866,10.35371 -15.90933,5.36707 -4.08421,17.99764 3.77771,4.03281 6.86881,7.51225 6.86912,7.73209 6.7e-4,0.44109 -3.13868,6.29604 -14.61801,27.26333 l -7.66383,13.99817 -85.66337,0.0839 -85.66337,0.084 z m 36.53005,-98.29151 c 2.37478,-0.45061 5.97025,-1.96322 8.22676,-3.46101 1.01346,-0.67269 2.9004,-2.36928 4.1932,-3.77019 4.58164,-4.96478 6.24856,-10.26763 5.38425,-17.12853 -0.57442,-4.55975 -2.24363,-7.75289 -6.03034,-11.53581 -3.19364,-3.19045 -5.50196,-4.7014 -8.9803,-5.87821 -6.65353,-2.25105 -13.74507,-1.8243 -20.01153,1.20424 -2.25319,1.08895 -3.36428,1.92331 -5.86238,4.40226 -4.00216,3.9715 -5.67936,7.13069 -6.26853,11.80752 -0.91779,7.28548 0.91736,12.66303 6.05661,17.74767 5.46826,5.41015 10.8317,7.4644 18.5198,7.09325 1.64979,-0.0796 3.7974,-0.29618 4.77246,-0.48119 z"
+         id="path6477"
+         transform="scale(0.26458333)"
+         sodipodi:nodetypes="sssssssccsscccsssssssssssss"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <path
+         style="fill:#000000;stroke:#000000;stroke-width:0.0833224;stroke-opacity:1"
+         d="m 523.55724,587.78557 c -0.003,-0.34181 -2.40837,-3.00763 -8.78566,-9.73623 -4.00666,-4.22738 -4.82538,-5.11587 -5.86624,-6.3662 -0.43037,-0.51698 -0.51357,-0.6681 -0.51357,-0.93274 0,-0.21135 0.062,-0.38344 0.18747,-0.52032 0.19341,-0.211 2.11168,-1.42133 7.01992,-4.42922 4.40224,-2.69781 5.13374,-3.21269 5.27282,-3.71134 0.0576,-0.20652 0.27992,-0.0474 5.99438,4.2898 4.04076,3.06688 5.92575,4.54583 5.90693,4.63451 -0.0241,0.11375 -8.50304,15.65775 -9.01905,16.53424 -0.10792,0.18331 -0.19657,0.29018 -0.197,0.2375 z"
+         id="path7568"
+         transform="scale(0.26458333)"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#ffffff;stroke:none;stroke-width:0.155068;stroke-opacity:1"
+         id="path7843-7"
+         cx="88.352768"
+         cy="109.67966"
+         rx="7.378705"
+         ry="6.6288304"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#ffffff;stroke:none;stroke-width:0.155068;stroke-opacity:1"
+         id="path7843-7-9"
+         cx="64.350075"
+         cy="130.32138"
+         rx="7.378705"
+         ry="6.6288304"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#ffffff;stroke:none;stroke-width:0.155068;stroke-opacity:1"
+         id="path7843-7-2"
+         cx="78.068115"
+         cy="113.4856"
+         rx="7.378705"
+         ry="6.6288304"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#ffffff;stroke:none;stroke-width:0.155068;stroke-opacity:1"
+         id="path7843-7-0"
+         cx="70.585548"
+         cy="120.71875"
+         rx="7.378705"
+         ry="6.6288304"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+      <ellipse
+         style="fill:#ffffff;stroke:none;stroke-width:0.155068;stroke-opacity:1"
+         id="path7843-7-97"
+         cx="63.477116"
+         cy="142.4182"
+         rx="7.378705"
+         ry="6.6288304"
+         inkscape:export-filename="C:\Users\abeld\Desktop\Sheep assets\path7843-6.png"
+         inkscape:export-xdpi="96"
+         inkscape:export-ydpi="96" />
+    </g>
+  </svg>
+  );
+};
+
+export default Icon;
